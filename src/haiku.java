9feffa60860347d6cad9d2dd157e48d95e37e25e
Matty-day <30204301@nescol.ac.uk>
2023-08-30 12:24:17 +0100

    added to Helloworld program

---
+++ src/haiku.java
@@ -0,0 +1,11 @@
+public class haiku {
+
+    public static void main(String[] args) {
+        System.out.println("Golden orb ascends");
+        System.out.println("Painting skies with hues divine");
+        System.out.println("Day's canvas unveiled");
+        //Golden orb ascends,
+        //Painting skies with hues divine,
+        //Day's canvas unveiled.
+    }
+}
