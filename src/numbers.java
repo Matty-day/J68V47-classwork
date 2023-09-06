7ab223bb81575c040fe07680a7581976de185bdb
Matty-day <30204301@nescol.ac.uk>
2023-09-06 10:07:39 +0100

    added to Helloworld program

---
+++ src/numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class numbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter number 1");
+        int num = input.nextInt();
+
+        System.out.print("enter number 2");
+        double price = input.nextDouble();
+
+        System.out.format("total = £%.2f%n",(price*num)+(price/num)+(num-price));
+    }
+}
