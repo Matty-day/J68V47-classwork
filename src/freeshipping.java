7e34b22d55e72eb569cb651e15133b63edc944f7
Matty-day <30204301@nescol.ac.uk>
2023-09-20 11:59:28 +0100

    added to Helloworld program

---
+++ src/freeshipping.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class freeshipping {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter total purchase amount £");
+        float num1 = input.nextFloat();
+
+        if (num1<50){
+            System.out.println("Shipping cost is = £10.00");
+            System.out.println("your final total is = £" + (num1+10));
+        } else{
+            System.out.println("Shipping cost is free");
+            System.out.println("your final total is = £" + (num1));
+    }
+}
+}
