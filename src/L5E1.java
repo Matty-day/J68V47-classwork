e47bf3f2513ef7a23c62ff9d3e1cc88b86577c7d
Matty-day <30204301@nescol.ac.uk>
2023-09-27 10:29:42 +0100

    added to Helloworld program

---
+++ src/L5E1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class L5E1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number ");
+        Integer n3 = input.nextInt();
+
+    for( int count=1; count<=12; count++ )
+    {
+        System.out.println(count*n3);
+    }
+
+    }
+
+}
---
+++ src/L5E2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class L5E2 {
+    public static void main(String[] args) {
+
+
+        for( int count=1; count<=10; count+=1 )
+        {
+            System.out.println(count);
+        }
+
+        System.out.println("blastoff !!!!!!!");
+
+        }
+
+    }
+
